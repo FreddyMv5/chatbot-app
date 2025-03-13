c0d48d14b9146b218bd49150feec1a94fc9edde7
FreddyMv5 <freddymanuelvillarabreu@gmail.com>
2025-03-12 23:22:48 -0400

    thirth pull

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,6 @@
-import React from 'react';
+import React from "react";
+import { auth, provider, signInWithPopup } from "../firebaseConfig";
 import "../styles/Login.css";
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBIcon
-} from 'mdb-react-ui-kit';
-import { auth, provider, signInWithPopup } from '../firebaseConfig';
 
 const Login = ({ setUser }) => {
   const signInWithGoogle = () => {
@@ -17,34 +8,22 @@ const Login = ({ setUser }) => {
       .then((result) => {
         setUser(result.user);
       })
-      .catch((error) => {
-        console.error("Error al iniciar sesión con Google:", error);
-      });
+      .catch((error) => console.error("Error al iniciar sesión con Google:", error));
   };
 
   return (
-    <MDBContainer fluid className="login-bg">
-      <MDBRow className="d-flex justify-content-center align-items-center h-100">
-        <MDBCol col="12" md="8" lg="6" xl="5">
-          <MDBCard className="bg-dark text-white my-5 mx-auto" style={{ borderRadius: '1rem' }}>
-            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
-              
-              {/* Título principal */}
-              <h2 className="fw-bold mb-2 text-uppercase">BillarBot</h2>
-              {/* Subtítulo */}
-              <p className="text-white-50 mb-5">Inicia sesión con Google</p>
-              
-              {/* Botón para iniciar sesión con Google */}
-              <MDBBtn onClick={signInWithGoogle} outline className="mx-2 px-5" color="white" size="lg">
-                <MDBIcon fab icon="google" size="lg" className="me-2" />
-                Iniciar con Google
-              </MDBBtn>
-
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div className="login-container">
+      <h1>BillarBot</h1>
+      <h3>Inicia sesión con Google</h3>
+      <button className="google-button" onClick={signInWithGoogle}>
+        <img
+          src="/images/google-icon.png"
+          alt="Google Icon"
+          className="google-icon"
+        />
+        Iniciar con Google
+      </button>
+    </div>
   );
 };
 
